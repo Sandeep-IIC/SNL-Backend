5aad31605b3c993626a1873e157e288eea0bfc37
Sandeep-IIC <sandeep.sharma.iic.0120@gmail.com>
2025-09-05 20:20:06 +0530

    Getting Position

---
+++ constants.js
@@ -0,0 +1,20 @@
+export const Events = {
+    SET_POSITION: "set-position",
+    GET_POSITION: "get-position",
+    JOIN_ROOM: "join-room",
+    LEAVE_ROOM: "leave-room",
+    CREATE_ROOM: "create-room",
+    GET_ROOM: "get-room",
+    GET_ROOMS_USERS: "get-rooms-users",
+    DICE_NUMBER: "dice-number",
+    GET_USER_CHANCE : "get-user-chance",
+    GET_ROOM_USER_POSITION : "get-room-user-position",
+}
+
+export const ReturnEvents = {
+    ROOM_CREATED: "room-created",
+    ROOM_JOINED: "room-joined",
+    ROOM_FULL: "room-full",
+    ROOMS: "rooms",
+    ROOMS_USERS: "rooms-users"
+}
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const http = require("http");
 const WebSocket = require("ws");
 const redisClient = require("./redis-connection");
 const app = express();
+const { Events, ReturnEvents } = require("./constants");
 
 app.get("/", (req, res) => {
   res.send("Hello World! asdfasf");
@@ -13,17 +14,158 @@ const server = http.createServer(app);
 const webSocketServer = new WebSocket.Server({ server });
 
 webSocketServer.on("connection", async (ws, req) => {
+  ws.on("message", async (message) => {
+    let data = JSON.parse(message);
+
+    if (data.event === Events.DICE_NUMBER) {
+      const userId = data.payload.userId;
+      webSocketServer.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(
+            JSON.stringify({
+              event: Events.DICE_NUMBER,
+              payload: { userId: userId, number: data.payload.number },
+            })
+          );
+        }
+      });
+    }
+
+    if (data.event === Events.SET_POSITION) {
+      const userId = data.payload.userId;
+      const roomId = data.payload.userId;
+      await redisClient.set(
+        userId.toString(),
+        data.payload.position.toString()
+      );
+      webSocketServer.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(
+            JSON.stringify({
+              event: Events.SET_POSITION,
+              payload: { position: data.payload.position, userId: userId },
+            })
+          );
+        }
+      });
+    }
+
+    if (data.event === Events.GET_POSITION) {
+      const userId = data.payload.userId;
+      await redisClient.get(userId).then((res) => {
+        ws.send(res);
+      });
+    }
+
+    if (data.event === Events.CREATE_ROOM) {
+      const roomId = data.payload.roomId;
+      await redisClient.sAdd("rooms", roomId);
+      ws.send(
+        JSON.stringify({
+          event: ReturnEvents.ROOM_CREATED,
+          payload: { roomId },
+        })
+      );
+    }
+
+    if (data.event === Events.JOIN_ROOM) {
+      console.log("Join Room", data);
+      const roomId = data.payload.roomId;
+      const userId = data.payload.userId;
+      await redisClient.sMembers(`room-${roomId}-users`).then(async (res) => {
+        if (res.length === 4) {
+          ws.send(
+            JSON.stringify({
+              event: ReturnEvents.ROOM_FULL,
+              payload: { roomId },
+            })
+          );
+          return;
+        } else {
+          await redisClient.sAdd(`room-${roomId}-users`, userId);
+          ws.send(
+            JSON.stringify({
+              event: ReturnEvents.ROOM_JOINED,
+              payload: { roomId },
+            })
+          );
+        }
+      });
+    }
+
+    if (data.event === Events.GET_ROOMS_USERS) {
+      const roomId = data.payload.roomId;
+      await redisClient.sMembers(`room-${roomId}-users`).then((res) => {
+        ws.send(
+          JSON.stringify({
+            event: ReturnEvents.ROOMS_USERS,
+            payload: { users: res },
+          })
+        );
+      });
+    }
+
+    if (data.event === Events.GET_ROOM) {
+      await redisClient.sMembers("rooms").then(async (res) => {
+        const roomUserMapEntries = await Promise.all(
+          res.map(async (roomId) => {
+            const users = await redisClient.sMembers(`room-${roomId}-users`);
+            return [roomId, users];
+          })
+        );
+
+        const roomUserMap = new Map(roomUserMapEntries);
+
+        const rooms = res.map((roomId) => {
+          return {
+            roomId,
+            users: roomUserMap.get(roomId) || [],
+          };
+        });
+
+        ws.send(
+          JSON.stringify({
+            event: ReturnEvents.ROOMS,
+            payload: { rooms },
+          })
+        );
+      });
+    }
+
+    if (data.event === Events.GET_ROOM_USER_POSITION) {
+      const roomId = data.payload.roomId;
+      await redisClient.sMembers(`room-${roomId}-users`).then(async (res) => {
+        const roomUserPositionMapEntries = await Promise.all(
+          res.map(async (userId) => {
+            const userPosition = await redisClient.get(userId);
+            return [userId, userPosition];
+          })
+        );
+
+        const roomUserPositionMap = new Map(roomUserPositionMapEntries);
+
+        const positions = res.map((userId) => {
+          return {
+            userId,
+            position: roomUserPositionMap.get(userId) || [],
+          };
+        });
+        ws.send(
+          JSON.stringify({
+            event: ReturnEvents.GET_USER_CHANCE,
+            payload: { positions },
+          })
+        );
+      });
+    }
+  });
+
   const url = new URL(req.url, `http://${req.headers.host}`);
   const userId = url.searchParams.get("userId");
 
   console.log("User connected:", userId);
   await redisClient.sAdd("onlineUsers", userId);
   const onlineUsers = await redisClient.sMembers("onlineUsers");
-
-  ws.on("message", (message) => {
-    console.log(`Received from ${userId}: ${message}`);
-    ws.send(`Echo: ${message}`);
-  });
 
   ws.on("close", async () => {
     console.log("User disconnected:", userId);
