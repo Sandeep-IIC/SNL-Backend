f5f8ebc87cbf025ec282aaed6fc57d1cd59f788a
Sandeep-IIC <sandeep.sharma.iic.0120@gmail.com>
2025-09-05 17:08:18 +0530

    Websocket done

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 const express = require("express");
 const port = 5000;
-const app = express();
+const http = require('http');
+const WebSocket = require("ws");
 const redisClient = require("./redis-connection");
+const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello World! asdfasf");
 });
 
-app.listen(port, () => {
+const server = http.createServer(app);
+const webSocketServer = new WebSocket.Server({ server });
+
+webSocketServer.on("connection", (ws) => {
+  console.log("Client connected");
+  webSocketServer.on("message", (message) => {
+    console.log(`Received: ${message}`);
+    webSocketServer.send(`Echo: ${message}`)
+  });
+  webSocketServer.on("close", () => console.log("Client disconnected"));
+});
+
+server.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
